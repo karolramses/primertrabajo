2aacbb3a6d6192184dc8c1d278a4293240b8cb9b
karolramses <131328597+karolramses@users.noreply.github.com>
2025-04-24 12:41:25 -0500

    Update ejercicio18.js

--- ejercicio18.js
+++ ejercicio18.js
@@ -1,7 +1,13 @@
-suma = 0
-while True:
-    numero = int(input("Ingresa un número (0 para terminar): "))
-    if numero == 0:
-        break
-    suma += numero
-print("La suma total es:", suma)
+let suma = 0;
+
+while (true) {
+  const numero = parseInt(prompt("Ingresa un número (0 para terminar):"));
+
+  if (numero === 0) {
+    break;
+  }
+
+  suma += numero;
+}
+
+console.log("La suma total es:", suma);
