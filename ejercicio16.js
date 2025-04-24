34994573a7370141584cd3a0a36233e2df538d21
karolramses <131328597+karolramses@users.noreply.github.com>
2025-04-24 12:40:16 -0500

    Update ejercicio16.js

--- ejercicio16.js
+++ ejercicio16.js
@@ -1,8 +1,13 @@
-suma = 0
-for i in range(1, 51):
-    if i % 2 != 0:
-        suma += i
-    while suma > 500:
-        print("La suma ha superado 500. Deteniendo...")
-        break
-print("Suma total de impares entre 1 y 50:", suma)
+let suma = 0;
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 !== 0) {
+    suma += i;
+  }
+  while (suma > 500) {
+    console.log("La suma ha superado 500. Deteniendo...");
+    break;
+  }
+}
+
+console.log("Suma total de impares entre 1 y 50:", suma);
